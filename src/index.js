9087fed26c5469733bd1ae087fdbfce4d26d184f
Parth <kparth2010@gmail.com>
2020-03-03 21:21:25 +0530

    made changes

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from  "react";
+import ReactDom from "react-dom";
+import App from "./components/App"
+
+ReactDom.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+
+
+
+
