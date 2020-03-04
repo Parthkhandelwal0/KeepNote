f4364e866c185e270df8aa651cb3f7b4beef51c6
Parth <kparth2010@gmail.com>
2020-03-04 20:54:22 +0530

    made changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
+
+function noteTerm(note){
+    return < Note 
+    key = {note.id}
+    id ={note.id}
+    title = {note.title}
+    content = {note.content}
+    />
+}
 
 function App(){
     return <div><Header />
+    {notes.map(noteTerm)}
     <Footer />
-    <Note />
+    
     </div>
 }
 
