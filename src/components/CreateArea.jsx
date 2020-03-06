f64f4f390b547d703bb8c0821fd15819fb267fb1
Parth <kparth2010@gmail.com>
2020-03-06 16:19:51 +0530

    made changes

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,37 +1,65 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
 
-  const [note, setNote]= useState({
-    title:"",
-    content:""
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
   });
-  function handleChange(event){
-    const {name, value}= event.target;
-    setNote((prevNote)=>{
-      return{
-      ...prevNote,
-      [name]:value
-      }
-    })
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
   }
 
-  function submitNote(event){
-
-    props.onAdd(note)
+  function submitNote(event) {
+    props.onAdd(note);
     setNote({
-      title:"",
-      content:""
-    })
+      title: "",
+      content: ""
+    });
     event.preventDefault();
+  }
+
+  function expand() {
+    setExpanded(true);
   }
 
   return (
     <div>
-      <form>
-        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
-        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote}>Add</button>
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddCircleOutlineIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
