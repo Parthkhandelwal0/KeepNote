3a3f1b41bdb8476f112c1b844da20c2134eb82e1
Parth <kparth2010@gmail.com>
2020-03-06 14:14:34 +0530

    made changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,18 @@
 import React from "react";
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
-import notes from "../notes"
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-function noteTerm(note){
-    return < Note 
-    key = {note.id}
-    id ={note.id}
-    title = {note.title}
-    content = {note.content}
-    />
-}
-
-function App(){
-    return <div><Header />
-    {notes.map(noteTerm)}
-    <Footer />
-    
+function App() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
+  );
 }
 
 export default App;
